cd9984c672da6b26d3b07796c4d8aa87e1fb1a6e
Mahmoud-Khaled-FS <mahmoudkhaled1215@gmail.com>
2023-02-03 20:44:16 +0200

    v0.5beta

--- src/constants/answerMessages.ts
+++ src/constants/answerMessages.ts
@@ -8,7 +8,7 @@ Ex. M: Avengers Endgame
 
 For more commands, use /help command by default language "English", you can change it to "Arabic" with /setlang command
 
-#version 0.4beta
+#version 0.5beta
 Join our group to communicate with the programmer or report bugs.
 https://t.me/+Y6zhcvygMfgxNzc8
 `,
